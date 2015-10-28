c9ef9f82aa09b91d8d189b7b4a456eeabd1b93bc
joshuabamboo <joshuaowens011@gmail.com>
2015-10-28 10:24:35 -0400

    add tests

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
-def my_find()
-  
+def my_find(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    if yield(collection[i])
+      block_return_values << collection[i]
+    end
+    i = i + 1
+  end
+  block_return_values
 end
