a0300f720e0b52412bc6d1cee30da78eeb2fbc4c
joshuabamboo <joshuaowens011@gmail.com>
2015-10-30 14:41:55 -0400

    fix solution/tests to reflect #find

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,12 +2,8 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  block_return_values = []
   while i < collection.length
-    if yield(collection[i])
-      block_return_values << collection[i]
-    end
+    return collection[i] if yield(collection[i])
     i = i + 1
   end
-  block_return_values
 end
