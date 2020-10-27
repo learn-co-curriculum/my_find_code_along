c90f0164de20d72effa155ac54578ea4326579bd
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-10-27 03:13:08 -0500

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
-
+    i = 0 
+    while i < collection.length
+        return collection[i] if yield(collection[i])
+        i += 1
+    end
 end
+
+
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 } 
